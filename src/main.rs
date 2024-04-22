ffc8209e6019e582964905385e653c15df06206d
Emmanuel Briot <briot.emmanuel@gmail.com>
2024-04-22 13:23:52 +0200

    alpha-beta strategy

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 mod errors;
 use rand::rngs::ThreadRng;
 use rand::Rng;
+use std::collections::HashMap;
 use std::thread::available_parallelism;
 
 #[derive(Debug)]
 enum Action {
-    Put { is_player1: bool, mask: u16 },
+    Put { mask: u16 },
 }
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug)]
 enum Score {
     Player1Wins,
     Player2Wins,
@@ -18,25 +19,22 @@ enum Score {
 
 struct LegalMoves {
     occupied: u16, //  each bit is whether the corresponding cell is legal
-    is_player1: bool,
 }
 
 trait Strategy {
-    fn play(&mut self, legal: &LegalMoves, rng: &mut ThreadRng) -> Action;
+    fn play(&mut self, state: &GameState, rng: &mut ThreadRng) -> Action;
 }
 
 struct StrategyRandom {}
 impl Strategy for StrategyRandom {
-    fn play(&mut self, legal: &LegalMoves, rng: &mut ThreadRng) -> Action {
+    fn play(&mut self, state: &GameState, rng: &mut ThreadRng) -> Action {
+        let legal = state.legal_moves();
         let mut choice = rng.gen_range(0..legal.occupied.count_zeros());
         let mut current = 1;
         loop {
             if (legal.occupied & current) == 0 {
                 if choice == 0 {
-                    return Action::Put {
-                        is_player1: legal.is_player1,
-                        mask: current,
-                    };
+                    return Action::Put { mask: current };
                 }
                 choice -= 1;
             }
@@ -45,9 +43,80 @@ impl Strategy for StrategyRandom {
     }
 }
 
+#[derive(Default)]
+struct StrategyAlphaBeta {
+    cache: HashMap<GameState, (Score, u16)>,
+}
+impl StrategyAlphaBeta {
+    fn play_with_score(&mut self, state: &GameState) -> (Score, u16) {
+        // ??? Could go faster by checking symmetries and rotations
+        if let Some(b) = self.cache.get(state) {
+            return *b;
+        }
+
+        let legal = state.legal_moves();
+        let mut best_score: Score = Score::Unknown;
+        let mut best_play_mask: u16 = 0;
+        for current in 0..=8 {
+            let mask = 1 << current;
+            if (legal.occupied & mask) == 0 {
+                let next_state = state.perform(Action::Put { mask });
+                let mut score = next_state.score();
+                if let Score::Unknown = score {
+                    let (s, _) = self.play_with_score(&next_state);
+                    score = s;
+                }
+
+                match (state.is_player1, score, best_score) {
+                    (true, Score::Player1Wins, _) => {
+                        return (Score::Player1Wins, mask);
+                    }
+                    (false, Score::Player1Wins, Score::Unknown) => {
+                        best_score = Score::Player1Wins;
+                        best_play_mask = mask;
+                    }
+                    (true, Score::Player2Wins, Score::Unknown) => {
+                        best_score = Score::Player2Wins;
+                        best_play_mask = mask;
+                    }
+                    (false, Score::Player2Wins, _) => {
+                        return (Score::Player2Wins, mask);
+                    }
+                    (true, Score::Draw, Score::Player2Wins)
+                    | (false, Score::Draw, Score::Player1Wins)
+                    | (_, Score::Draw, Score::Unknown) => {
+                        // draw is better than letting the other player win
+                        best_score = Score::Draw;
+                        best_play_mask = mask;
+                    }
+                    (false, Score::Player1Wins, _)
+                    | (true, Score::Player2Wins, _)
+                    | (_, Score::Draw, _) => {
+                        // We already have a better strategy, ignore this one
+                    }
+                    (_, Score::Unknown, _) => {
+                        panic!("should not happen");
+                    }
+                }
+            }
+        }
+        self.cache.insert(*state, (best_score, best_play_mask));
+        (best_score, best_play_mask)
+    }
+}
+
+impl Strategy for StrategyAlphaBeta {
+    fn play(&mut self, state: &GameState, _: &mut ThreadRng) -> Action {
+        let (_, m) = self.play_with_score(state);
+        Action::Put { mask: m }
+    }
+}
+
+#[derive(Clone, Copy, Eq, Hash, PartialEq)]
 struct GameState {
-    player1: u16, // bit set to 1 if player1 occupies the cell
-    player2: u16, // bit set to 1 if player1 occupies the cell
+    player1: u16,     // bit set to 1 if player1 occupies the cell
+    player2: u16,     // bit set to 1 if player1 occupies the cell
+    is_player1: bool, // true if next to play is player1
 }
 
 impl Default for GameState {
@@ -55,32 +124,35 @@ impl Default for GameState {
         GameState {
             player1: !0b111111111, // so that count_zeros only looks at board
             player2: !0b111111111,
+            is_player1: true,
         }
     }
 }
 
 impl GameState {
-    pub fn perform(self, action: Action) -> Self {
+    pub fn perform(&self, action: Action) -> Self {
         match action {
-            Action::Put { is_player1, mask } => {
-                let mut next = GameState {
-                    player1: self.player1,
-                    player2: self.player2,
-                };
-                if is_player1 {
-                    next.player1 |= mask;
+            Action::Put { mask } => {
+                if self.is_player1 {
+                    GameState {
+                        player1: self.player1 | mask,
+                        player2: self.player2,
+                        is_player1: false,
+                    }
                 } else {
-                    next.player2 |= mask;
+                    GameState {
+                        player1: self.player1,
+                        player2: self.player2 | mask,
+                        is_player1: true,
+                    }
                 }
-                next
             }
         }
     }
 
-    pub fn legal_moves(&self, is_player1: bool) -> LegalMoves {
+    pub fn legal_moves(&self) -> LegalMoves {
         LegalMoves {
             occupied: self.player1 | self.player2,
-            is_player1,
         }
     }
 
@@ -145,15 +217,13 @@ fn play<Strategy1: Strategy, Strategy2: Strategy>(
 
     loop {
         let mut state = GameState::default();
-        let mut is_player1 = true;
         played += 1;
 
         loop {
-            let legal = state.legal_moves(is_player1);
-            let action = if is_player1 {
-                player1.play(&legal, &mut rng)
+            let action = if state.is_player1 {
+                player1.play(&state, &mut rng)
             } else {
-                player2.play(&legal, &mut rng)
+                player2.play(&state, &mut rng)
             };
 
             state = state.perform(action);
@@ -172,8 +242,6 @@ fn play<Strategy1: Strategy, Strategy2: Strategy>(
                 }
                 Score::Unknown => {}
             }
-
-            is_player1 = !is_player1;
         }
 
         if played >= max_count {
@@ -187,16 +255,27 @@ fn main() -> Result<(), crate::errors::Error> {
     const TOTAL_RUNS: u32 = 1_000_000;
     let cores: u32 = available_parallelism().unwrap().get().try_into().unwrap();
 
+    // https://math.stackexchange.com/questions/4045893/if-two-computers-are-playing-tic-tac-toe-but-they-are-choosing-their-squares-ra
     // Random vs Random:
     //    theory says 58.49% of wins for player1, 28.81% for player1,
     //    and 12.70% draw.
+    // Random vs Perfect:
+    //    if first player is perfect:
+    //       wins with 191/192 = 99.48% of wins
+    //       draws with 1/192 = 0.52%
+    //    if second player is perfect:
+    //       wins with 887/945 = 93.86%
+    //       draws with 43/945 =  4.55%
+    //       loses with 1/945  =  1.06%
 
     let handles = (0..cores)
-        .map(|_| std::thread::spawn(move || {
-            let mut player1 = StrategyRandom {};
-            let mut player2 = StrategyRandom {};
-            play(TOTAL_RUNS / cores, &mut player1, &mut player2)
-        }))
+        .map(|_| {
+            std::thread::spawn(move || {
+                let mut player1 = StrategyRandom {};
+                let mut player2 = StrategyAlphaBeta::default();
+                play(TOTAL_RUNS / cores, &mut player1, &mut player2)
+            })
+        })
         .collect::<Vec<_>>();
 
     let mut played = 0;
