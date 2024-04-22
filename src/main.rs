947d7b51e2228bac7dcde8da1a7b0fd9c64a3a6f
Emmanuel Briot <briot.emmanuel@gmail.com>
2024-04-22 09:00:24 +0200

    Initial version, random player

---
+++ src/main.rs
@@ -0,0 +1,178 @@
+mod errors;
+use rand::seq::SliceRandom;
+
+type Player = i8;
+
+enum Action {
+    Put { player: Player, cell: usize },
+}
+
+#[derive(Debug)]
+enum Score {
+    Player1Wins,
+    Player2Wins,
+    Draw,
+    Unknown,
+}
+
+#[derive(Default)]
+struct GameState {
+    board: [i8; 9],
+}
+impl GameState {
+    pub fn perform(self, action: &Action) -> Self {
+        match action {
+            Action::Put { player, cell } => {
+                let mut b = self.board;
+                b[*cell] = *player;
+                GameState { board: b }
+            }
+        }
+    }
+
+    pub fn legal_actions(&self, player: Player) -> Vec<Action> {
+        let mut res = Vec::new();
+        for cell in 0_usize..9 {
+            if self.board[cell] == 0 {
+                res.push(Action::Put { player, cell });
+            }
+        }
+        res
+    }
+
+    pub fn score(&self) -> Score {
+        let row1 = self.board[0] + self.board[1] + self.board[2];
+        if row1 == 3 {
+            return Score::Player1Wins;
+        } else if row1 == -3 {
+            return Score::Player2Wins;
+        }
+
+        let row2 = self.board[3] + self.board[4] + self.board[5];
+        if row2 == 3 {
+            return Score::Player1Wins;
+        } else if row2 == -3 {
+            return Score::Player2Wins;
+        }
+
+        let row3 = self.board[6] + self.board[7] + self.board[8];
+        if row3 == 3 {
+            return Score::Player1Wins;
+        } else if row3 == -3 {
+            return Score::Player2Wins;
+        }
+
+        let col1 = self.board[0] + self.board[3] + self.board[6];
+        if col1 == 3 {
+            return Score::Player1Wins;
+        } else if col1 == -3 {
+            return Score::Player2Wins;
+        }
+
+        let col2 = self.board[1] + self.board[4] + self.board[7];
+        if col2 == 3 {
+            return Score::Player1Wins;
+        } else if col2 == -3 {
+            return Score::Player2Wins;
+        }
+
+        let col3 = self.board[2] + self.board[5] + self.board[8];
+        if col3 == 3 {
+            return Score::Player1Wins;
+        } else if col3 == -3 {
+            return Score::Player2Wins;
+        }
+
+        let diag1 = self.board[0] + self.board[4] + self.board[8];
+        if diag1 == 3 {
+            return Score::Player1Wins;
+        } else if diag1 == -3 {
+            return Score::Player2Wins;
+        }
+
+        let diag2 = self.board[6] + self.board[4] + self.board[2];
+        if diag2 == 3 {
+            return Score::Player1Wins;
+        } else if diag2 == -3 {
+            return Score::Player2Wins;
+        }
+
+        for j in 0..9 {
+            if self.board[j] == 0 {
+                return Score::Unknown;
+            }
+        }
+
+        Score::Draw
+    }
+}
+
+impl std::fmt::Display for GameState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f)?;
+        writeln!(
+            f,
+            "{:2} {:2} {:2}",
+            self.board[0], self.board[1], self.board[2]
+        )?;
+        writeln!(
+            f,
+            "{:2} {:2} {:2}",
+            self.board[3], self.board[4], self.board[5]
+        )?;
+        writeln!(
+            f,
+            "{:2} {:2} {:2}",
+            self.board[6], self.board[7], self.board[8]
+        )?;
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), crate::errors::Error> {
+    let mut rng = rand::thread_rng();
+    let mut play1wins = 0;
+    let mut play2wins = 0;
+    let mut draw = 0;
+    let mut played = 0;
+    loop {
+        let mut state = GameState::default();
+        let mut player: Player = 1;
+        played += 1;
+
+        loop {
+            let actions = state.legal_actions(player);
+            let action = actions.choose(&mut rng).unwrap();
+            state = state.perform(action);
+            match state.score() {
+                Score::Player1Wins => {
+                    play1wins += 1;
+                    break;
+                }
+                Score::Player2Wins => {
+                    play2wins += 1;
+                    break;
+                }
+                Score::Draw => {
+                    draw += 1;
+                    break;
+                }
+                Score::Unknown => {}
+            }
+
+            player = -player;
+        }
+
+        if played >= 1_000_000 {
+            break;
+        }
+    }
+    println!(
+        "Played {}, play1 {:.2}%, play2 {:.2}%, draw {:.2}%",
+        played,
+        play1wins as f32 / played as f32 * 100.,
+        play2wins as f32 / played as f32 * 100.,
+        draw as f32 / played as f32 * 100.
+    );
+    Ok(())
+}
